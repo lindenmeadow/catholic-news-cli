5f6d375fa6853bf52253e27058e3b4c8bccbf925
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-08-10 19:21:05 +0000

    first commit

---
+++ lib/catholic/news/cli.rb
@@ -0,0 +1,10 @@
+require "catholic/news/cli/version"
+
+module Catholic
+  module News
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/catholic/news/cli/version.rb
@@ -0,0 +1,7 @@
+module Catholic
+  module News
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end
